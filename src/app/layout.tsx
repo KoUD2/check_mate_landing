4379cb6ab2a37a2dafc5bb6ef7e347452ef2ad3b
Konstantin Udod <kostik-udod@mail.ru>
2026-04-06 18:37:40 +0300

    fix: correct favicon svg path from icon.svg to favicon.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -171,7 +171,7 @@ export default function RootLayout({
 
         {/* Favicon and Apple Touch Icons */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
